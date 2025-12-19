010534ede4f86761d2358b9951051dd2b7eb7ec3
EkaterynaKuznetsova <kate7smith12@gmail.com>
2025-12-19 15:31:47 +0100

    Add tiktoken fallback

--- gpt_oss/tokenizer.py
+++ gpt_oss/tokenizer.py
@@ -1,25 +1,34 @@
 import tiktoken
 
 def get_tokenizer():
-    o200k_base = tiktoken.get_encoding("o200k_base")
+    # Try o200k_base (newer tiktoken versions), fallback to cl100k_base
+    try:
+        base_encoding = tiktoken.get_encoding("o200k_base")
+        base_vocab_size = 200000
+    except ValueError:
+        # Fallback for older tiktoken versions
+        base_encoding = tiktoken.get_encoding("cl100k_base")
+        base_vocab_size = 100000
+        print("Warning: o200k_base not found, using cl100k_base fallback. Consider upgrading tiktoken.")
+    
     tokenizer = tiktoken.Encoding(
         name="o200k_harmony",
-        pat_str=o200k_base._pat_str,
-        mergeable_ranks=o200k_base._mergeable_ranks,
+        pat_str=base_encoding._pat_str,
+        mergeable_ranks=base_encoding._mergeable_ranks,
         special_tokens={
-            **o200k_base._special_tokens,
-            "<|startoftext|>": 199998,
-            "<|endoftext|>": 199999,
-            "<|reserved_200000|>": 200000,
-            "<|reserved_200001|>": 200001,
-            "<|return|>": 200002,
-            "<|constrain|>": 200003,
-            "<|reserved_200004|>": 200004,
-            "<|channel|>": 200005,
-            "<|start|>": 200006,
-            "<|end|>": 200007,
-            "<|message|>": 200008,
-            "<|reserved_200009|>": 200009,
+            **base_encoding._special_tokens,
+            "<|startoftext|>": base_vocab_size - 2,
+            "<|endoftext|>": base_vocab_size - 1,
+            "<|reserved_200000|>": base_vocab_size,
+            "<|reserved_200001|>": base_vocab_size + 1,
+            "<|return|>": base_vocab_size + 2,
+            "<|constrain|>": base_vocab_size + 3,
+            "<|reserved_200004|>": base_vocab_size + 4,
+            "<|channel|>": base_vocab_size + 5,
+            "<|start|>": base_vocab_size + 6,
+            "<|end|>": base_vocab_size + 7,
+            "<|message|>": base_vocab_size + 8,
+            "<|reserved_200009|>": base_vocab_size + 9,
             "<|reserved_200010|>": 200010,
             "<|reserved_200011|>": 200011,
             "<|call|>": 200012,
